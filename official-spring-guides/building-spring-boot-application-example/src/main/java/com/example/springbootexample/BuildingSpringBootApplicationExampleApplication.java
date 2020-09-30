110150e1355c6559f73fa0567cf3ab39dfe275f6
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-09-30 17:20:58 +0200

    Add Controller and CommandLineRunner
    re:  https://spring.io/guides/gs/spring-boot/

--- official-spring-guides/building-spring-boot-application-example/src/main/java/com/example/springbootexample/BuildingSpringBootApplicationExampleApplication.java
+++ official-spring-guides/building-spring-boot-application-example/src/main/java/com/example/springbootexample/BuildingSpringBootApplicationExampleApplication.java
@@ -1,13 +1,27 @@
 package com.example.springbootexample;
 
+import java.util.Arrays;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BuildingSpringBootApplicationExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuildingSpringBootApplicationExampleApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner inspectBeanCommandLineRunner(ApplicationContext ctx){
+    return args -> {
+      System.out.println("Inspecting beans loaded in Spring Context:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			Arrays.stream(beanNames).forEach(System.out::println);
+    };
 	}
 
 }
