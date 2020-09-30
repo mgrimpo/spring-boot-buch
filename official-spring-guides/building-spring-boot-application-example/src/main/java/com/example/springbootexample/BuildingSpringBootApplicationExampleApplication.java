24584cf90841b409812c5ae5dd6b2d68fa136856
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-09-30 16:57:05 +0200

    Add spring.io/guides/gs/spring-boot/ project

---
+++ official-spring-guides/building-spring-boot-application-example/src/main/java/com/example/springbootexample/BuildingSpringBootApplicationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingSpringBootApplicationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingSpringBootApplicationExampleApplication.class, args);
+	}
+
+}
