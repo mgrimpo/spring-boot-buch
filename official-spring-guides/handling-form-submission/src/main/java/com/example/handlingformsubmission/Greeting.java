f8cf28387cb56984ce05153fff3cce6e276c2597
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-10-01 12:48:53 +0200

    Add Views & Controllers to handle form submission
    re: https://spring.io/guides/gs/handling-form-submission/

---
+++ official-spring-guides/handling-form-submission/src/main/java/com/example/handlingformsubmission/Greeting.java
@@ -0,0 +1,26 @@
+package com.example.handlingformsubmission;
+
+public class Greeting {
+
+
+  private long id;
+  private String content;
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+}
