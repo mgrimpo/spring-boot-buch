3c731d1cb014be00cbf8c6a61e3b37596216f466
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-10-17 12:32:48 +0200

    Add simple pair of Controller & Angular component
    re: https://spring.io/guides/tutorials/spring-security-and-angular-js/

--- official-spring-guides/spring-security-angular/src/main/java/com/example/springsecurityangular/SpringSecurityAngularApplication.java
+++ official-spring-guides/spring-security-angular/src/main/java/com/example/springsecurityangular/SpringSecurityAngularApplication.java
@@ -1,13 +1,27 @@
 package com.example.springsecurityangular;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringSecurityAngularApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityAngularApplication.class, args);
 	}
+
+	@GetMapping("/resource")
+  public Map<String, Object> resourceHandler(){
+	  Map<String, Object> model = new HashMap<>();
+	  model.put("id", UUID.randomUUID().toString());
+    model.put("content", "Hello from the server");
+	  return model;
+  }
 
 }
