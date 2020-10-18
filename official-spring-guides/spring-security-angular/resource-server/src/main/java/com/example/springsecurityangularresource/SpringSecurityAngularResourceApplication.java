ab2b4af2d812fddac84f77c5b6e6cc9944836f2f
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-10-18 11:06:23 +0200

    Add Spring Boot project for resource server
    re: https://spring.io/guides/tutorials/spring-security-and-angular-js/

---
+++ official-spring-guides/spring-security-angular/resource-server/src/main/java/com/example/springsecurityangularresource/SpringSecurityAngularResourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityangularresource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAngularResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAngularResourceApplication.class, args);
+	}
+
+}
---
+++ official-spring-guides/spring-security-angular/resource-server/src/test/java/com/example/springsecurityangularresource/SpringSecurityAngularResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityangularresource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAngularResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
