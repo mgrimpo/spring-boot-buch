70f0fffcbcf1c5634e00a9e0c1165970f8e80351
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-10-19 13:41:13 +0200

    Restrict CORS to requests from ui-server
    re: https://spring.io/guides/tutorials/spring-security-and-angular-js/

--- official-spring-guides/spring-security-angular/resource-server/src/main/java/com/example/springsecurityangularresource/ResourceController.java
+++ official-spring-guides/spring-security-angular/resource-server/src/main/java/com/example/springsecurityangularresource/ResourceController.java
@@ -1,28 +1,17 @@
 package com.example.springsecurityangularresource;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(
-    origins = "*")
 @RestController
 public class ResourceController {
 
-//  @CrossOrigin(
-//      origins = "*",
-//      methods = {RequestMethod.OPTIONS, RequestMethod.GET},
-//      allowedHeaders = {"x-auth-token", "x-requested-with", "x-xsrf-token"})
   @GetMapping("/")
-  //  @CrossOrigin(
-  //      origins = "*"
-  //      maxAge = 3600,
-  //      allowedHeaders = {"x-auth-token", "x-requested-with", "x-xsrf-token"}
-  //      )
+  @CrossOrigin(
+      origins = "http://localhost:8080",
+      maxAge = 3600,
+      allowedHeaders = {"x-auth-token", "x-requested-with", "x-xsrf-token"})
   public Message resourceHandler() {
     return new Message("Howdy from the server!");
   }
