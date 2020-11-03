56604becf8172684a6de63f5da9bb9249f423547
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-11-03 13:45:18 +0100

    Secure resource-server behind Zuul gateway

---
+++ official-spring-guides/spring-security-angular/resource-server/src/main/java/com/example/springsecurityangularresource/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.springsecurityangularresource;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@Component
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.httpBasic().disable();
+    http.authorizeRequests().anyRequest().authenticated();
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+  }
+
+}
