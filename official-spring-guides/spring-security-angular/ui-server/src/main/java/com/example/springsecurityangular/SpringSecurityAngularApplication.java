9189fa817a91effc7009c57039d43f5448b8d11e
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-11-03 14:48:44 +0100

    Enable httptrace actuator for ui-server
    re: https://spring.io/guides/tutorials/spring-security-and-angular-js/

--- official-spring-guides/spring-security-angular/ui-server/src/main/java/com/example/springsecurityangular/SpringSecurityAngularApplication.java
+++ official-spring-guides/spring-security-angular/ui-server/src/main/java/com/example/springsecurityangular/SpringSecurityAngularApplication.java
@@ -1,8 +1,11 @@
 package com.example.springsecurityangular;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,5 +13,11 @@ public class SpringSecurityAngularApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringSecurityAngularApplication.class, args);
+  }
+
+  @Bean
+  public HttpTraceRepository htttpTraceRepository()
+  {
+    return new InMemoryHttpTraceRepository();
   }
 }
