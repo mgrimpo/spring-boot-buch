0177f83cb6fe67611aed8a848bafbc5911f5eaec
Marc Grimpo <mgrimpo.dev@gmail.com>
2020-11-03 15:05:16 +0100

    Enable httptrace actuator for resource-server
    re: https://spring.io/guides/tutorials/spring-security-and-angular-js/

--- official-spring-guides/spring-security-angular/resource-server/src/main/java/com/example/springsecurityangularresource/SpringSecurityAngularResourceApplication.java
+++ official-spring-guides/spring-security-angular/resource-server/src/main/java/com/example/springsecurityangularresource/SpringSecurityAngularResourceApplication.java
@@ -4,15 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecurityAngularResourceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityAngularResourceApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityAngularResourceApplication.class, args);
+  }
 
+  @Bean
+  public HttpTraceRepository htttpTraceRepository() {
+    return new InMemoryHttpTraceRepository();
+  }
 }
